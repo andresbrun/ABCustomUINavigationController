c689779883af3598205fed30d7390829b9ba5cb9
andresbrun <andresbrunmoreno@gmail.com>
2013-08-09 10:44:53 +0200

    Folders sorting

--- ABCustomUINavigationControllerExample/AppDelegate.m
+++ ABCustomUINavigationControllerExample/AppDelegate.m
@@ -19,8 +19,8 @@
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
     self.viewController = [[ViewController alloc] initViewController];
-    //self.window.rootViewController = [[FlipSquaresNavigationController alloc] initWithRootViewController:self.viewController];
-    self.window.rootViewController = [[CubeNavigationController alloc] initWithRootViewController:self.viewController];
+    self.window.rootViewController = [[FlipSquaresNavigationController alloc] initWithRootViewController:self.viewController];
+    //self.window.rootViewController = [[CubeNavigationController alloc] initWithRootViewController:self.viewController];
     [self.window makeKeyAndVisible];
     
     return YES;
