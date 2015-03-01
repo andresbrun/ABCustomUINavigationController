2c4be3fc1519d83340bb2fade12a146ed4b184cf
Andres Brun Moreno <andresbrunmoreno@gmail.com>
2015-03-01 16:24:34 +0100

    Making ForwardingDelegate optional.

--- CustomUINavigationController/NavigationClasses/BaseNavigationControllerDelegate.m
+++ CustomUINavigationController/NavigationClasses/BaseNavigationControllerDelegate.m
@@ -28,36 +28,22 @@
 }
 
 - (void)navigationController:(UINavigationController *)navigationController willShowViewController:(UIViewController *)viewController animated:(BOOL)animated {
-    if (self.forwardDelegate) {
+    if (self.forwardDelegate && [self.forwardDelegate respondsToSelector:@selector(navigationController:willShowViewController:animated:)]) {
         [self.forwardDelegate navigationController:navigationController willShowViewController:viewController animated:animated];
     }
 }
 
 - (void)navigationController:(UINavigationController *)navigationController didShowViewController:(UIViewController *)viewController animated:(BOOL)animated {
-    if (self.forwardDelegate) {
+    if (self.forwardDelegate && [self.forwardDelegate respondsToSelector:@selector(navigationController:didShowViewController:animated:)]) {
         [self.forwardDelegate navigationController:navigationController didShowViewController:viewController animated:animated];
     }
 }
 
 - (id<UIViewControllerInteractiveTransitioning>)navigationController:(UINavigationController *)navigationController interactionControllerForAnimationController:(id<UIViewControllerAnimatedTransitioning>)animationController {
-    if (self.forwardDelegate) {
+    if (self.forwardDelegate && [self.forwardDelegate respondsToSelector:@selector(navigationController:interactionControllerForAnimationController:)]) {
         return [self.forwardDelegate navigationController:navigationController interactionControllerForAnimationController:animationController];
     }
     return nil;
-}
-
-- (UIInterfaceOrientation)navigationControllerPreferredInterfaceOrientationForPresentation:(UINavigationController *)navigationController {
-    if (self.forwardDelegate) {
-        return [self.forwardDelegate navigationControllerPreferredInterfaceOrientationForPresentation:navigationController];
-    }
-    return UIInterfaceOrientationPortrait;
-}
-
-- (NSUInteger)navigationControllerSupportedInterfaceOrientations:(UINavigationController *)navigationController {
-    if (self.forwardDelegate) {
-        return [self.forwardDelegate navigationControllerSupportedInterfaceOrientations:navigationController];
-    }
-    return UIInterfaceOrientationMaskPortrait;
 }
 
 @end
